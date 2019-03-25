65aa31109e775a71f75748197f3e8a78725a3475
Carlos Gómez <carlos.gomez@blautech.us>
2019-03-24 23:11:15 -0600

    Add styles for responsive mobile and menu option of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  state: string = 'open'
+  menuSelected: any = [false, true, false, false]
+
+  changeMenu(value: string) {
+    this.state = value;
+  }
+
+  changeSelectedMenu(position: number) {
+    if (position == 1) {
+      this.menuSelected = [true, false, false, false]
+    } else if (position == 2) {
+      this.menuSelected = [false, true, false, false]
+    } else if (position == 3) {
+      this.menuSelected = [false, false, true, false]
+    } else if (position == 4) {
+      this.menuSelected = [false, false, false, true]
+    }
+    this.state = 'menu';
+  }
+
+
 }
